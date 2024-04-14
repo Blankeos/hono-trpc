288def69b49aa01ed108ffe385724c9c1230ab40
Blankeos <carloantonioct@gmail.com>
2024-04-14 22:08:18 +0800

    feat: Added vike and renamed to +Page.tsx.

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import vike from "vike/plugin";
+import vikeSolid from "vike-solid/vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [vike(), vikeSolid()],
   /** Preview/Deployed server */
   preview: {
     port: 3000,
